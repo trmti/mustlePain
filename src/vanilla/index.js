d95e4d431da7fa3ce1e29a086279fa6d0148c6ee
trmti <trmtikko@gmail.com>
2022-01-09 19:42:33 +0900

    test

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -2,8 +2,8 @@ import './index.css';
 import liff from '@line/liff';
 import { db, getGroup } from './firebase';
 
-document.getElementById('firestore').innerHTML = getGroup(db).then((value) => {
-  return value[0].userIds[0];
+getGroup(db).then((data) => {
+  console.log(data[0]);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
