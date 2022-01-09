5d44c6999795e07bfaae9f3e6dc84089c4e02a26
trmti <trmtikko@gmail.com>
2022-01-09 17:22:56 +0900

    jpg画像追加

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
-      liff.login();
       const userId = liff.getContext();
       console.log(userId);
       const isLoggedIn = liff.isLoggedIn();
-      console.log(isLoggedIn);
+      if (!isLoggedIn) {
+        liff.login();
+      }
       document.getElementById('userId').innerText = isLoggedIn
         ? 'true'
         : 'false';
