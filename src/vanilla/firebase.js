8093c4ac529bdf4b68b992746d2cf958b109cd92
trmti <trmtikko@gmail.com>
2022-01-09 18:16:59 +0900

    fireBase連携

--- src/vanilla/firebase.js
+++ src/vanilla/firebase.js
@@ -1,9 +1,26 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-// Get a list of cities from your database
-export default async function getCities(db) {
-  const citiesCol = collection(db, 'group');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map((doc) => doc.data());
-  return cityList;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBueIcHSC1gVW4Y3xYHEc0yIkZuQwW8TDU',
+  authDomain: 'mustletrainapp.firebaseapp.com',
+  projectId: 'mustletrainapp',
+  storageBucket: 'mustletrainapp.appspot.com',
+  messagingSenderId: '861876161031',
+  appId: '1:861876161031:web:8c7c79a17dbdcc34e678a2',
+  measurementId: 'G-GQYRYMZDJK',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export async function getGroup(db) {
+  const groupCol = collection(db, 'group');
+  const groupSnapshot = await getDocs(groupCol);
+  const groupList = groupSnapshot.docs.map((doc) => doc.data());
+  return groupList;
 }
--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -1,25 +1,6 @@
 import './index.css';
 import liff from '@line/liff';
-import { getCities } from 'firebase';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp, getFirestore } from 'firebase/app';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyBueIcHSC1gVW4Y3xYHEc0yIkZuQwW8TDU',
-  authDomain: 'mustletrainapp.firebaseapp.com',
-  projectId: 'mustletrainapp',
-  storageBucket: 'mustletrainapp.appspot.com',
-  messagingSenderId: '861876161031',
-  appId: '1:861876161031:web:8c7c79a17dbdcc34e678a2',
-  measurementId: 'G-GQYRYMZDJK',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db, getGroup } from 'firebase';
 
 document.addEventListener('DOMContentLoaded', function () {
   liff
@@ -30,7 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
         liff.login();
       }
       const userId = liff.getContext().userId;
-      document.getElementById('firestore').innerHTML = getCities(db);
+      console.log(userId);
+      group = await getGroup(db);
+      document.getElementById('firestore').innerHTML = group[0].userIDs[0];
     })
     .catch((error) => {
       console.log(error);
