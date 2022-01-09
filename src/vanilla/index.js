18e563077279c695d4c5d72f35574ef041e9d9a5
trmti <trmtikko@gmail.com>
2022-01-09 20:41:43 +0900

    fireBase連携

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -3,12 +3,12 @@ import liff from '@line/liff';
 import { db, setTrainDay, setTrainTime } from './firebase';
 
 document.getElementById('btn-submit').addEventListener('click', () => {
+  liff.closeWindow();
   checkboxes = document.getElementsByClassName('trainingDay-checkbox');
   const groupId = liff.getContext().groupId;
   const trainDays = checkboxes.map((checkbox) => checkbox.checked === true);
   setTrainDay(db, groupId, trainDays);
   setTrainTime(db, groupId, document.time.value);
-  liff.closeWindow();
 });
 
 document.addEventListener('DOMContentLoaded', function () {
