3b804a574aaf49d03b82a2878027e694e6bdd9d5
trmti <trmtikko@gmail.com>
2022-01-10 20:46:07 +0900

    cloudFunctionと連携

---
+++ src/vanilla/functions/index.js
@@ -0,0 +1,36 @@
+const { utcToZonedTime } = require('date-fns-tz');
+const { format } = require('date-fns');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.sendReminder = functions
+  .region('asia-northeast1')
+  .pubsub.schedule('every 1 minutes')
+  .timeZone('Asia/Tokyo')
+  .onRun(async (context) => {
+    // 秒を切り捨てた現在時刻;
+    const now = (() => {
+      let s = admin.firestore.Timestamp.now().seconds;
+      s = s - (s % 60);
+      return new admin.firestore.Timestamp(s, 0);
+    })().toDate();
+    const nowTime = format(utcToZonedTime(now, 'Asia/Tokyo'), 'HH:mm');
+    const nowDate = format(utcToZonedTime(now, 'Asia/Tokyo'), 'eeee');
+    const remindersRef = await admin
+      .firestore()
+      .collection('group')
+      .where('TrainTime', '==', nowTime)
+      .where('TrainingDay', 'array-contains', nowDate)
+      .get();
+    const remindedGroupIds = remindersRef.docs.map((doc) => doc.id);
+    if (remindedGroupIds.length > 0) {
+      // TODO getアクション
+      console.log('リマインダー実行', remindedGroupIds);
+      return 'リマインダーがあります';
+    }
+
+    console.log('Ikkodayo', now, nowTime, nowDate);
+    return 'リマインダー通知処理終了';
+  });
